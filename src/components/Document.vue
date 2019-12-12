ac483884d8202005e25013c4a811ec34e4e2d85c
Rowan Udell <rowan.udell@versent.com.au>
2019-12-13 09:39:51 +1100

    feat(document): Emit JSON errors.

--- src/components/Document.vue
+++ src/components/Document.vue
@@ -1,21 +1,42 @@
 <template>
   <div id="document">
     <h1>Document</h1>
-    <textarea v-model="input"></textarea>
+    <textarea :value="input" @input="update"></textarea>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Document',
-  props: ['input'],
   data() {
-    return {}
+    return {
+      input: '',
+    }
   },
   created() {
+    // TODO: Do this in update lifecycle? Does it get run on create?
     this.$emit('isEmpty', true)
   },
-  // TODO: Debounce on input https://vuejs.org/v2/examples/index.html
+  computed: {
+    isValid: function() {
+      try {
+        const result = JSON.parse(this.input)
+        // TODO: Emit JSON
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+          this.$emit('error', e.message)
+          return false
+        }
+      }
+      return true
+    },
+  },
+  methods: {
+    // TODO: Debounce on input https://vuejs.org/v2/examples/index.html
+    update: function(e) {
+      this.input = e.target.value
+    },
+  },
 }
 </script>
 
--- tests/unit/components/Document.spec.js
+++ tests/unit/components/Document.spec.js
@@ -1,9 +1,33 @@
+import Vue from 'vue'
 import { shallowMount } from '@vue/test-utils'
 import Document from '@/components/Document.vue'
+
+// https://devdocs.io/javascript/errors/json_bad_parse
 
 describe('Document', () => {
   it('is empty when created', async () => {
     const wrapper = shallowMount(Document)
     expect(wrapper.emitted().isEmpty[0]).toEqual([true])
+  })
+
+  it('flags invalid JSON', async () => {
+    const wrapper = shallowMount(Document)
+    wrapper.setData({ input: 'OHAI' })
+    expect(wrapper.vm.isValid).toEqual(false)
+  })
+
+  it('allows valid JSON', async () => {
+    const wrapper = shallowMount(Document)
+    wrapper.setData({ input: '{}' })
+    expect(wrapper.vm.isValid).toEqual(true)
+  })
+
+  it('emits error on invalid JSON', async () => {
+    const wrapper = shallowMount(Document)
+    wrapper.setData({ input: 'OHAI' })
+    expect(wrapper.vm.isValid).toEqual(false)
+    expect(wrapper.emitted().error[0]).toEqual([
+      'Unexpected token O in JSON at position 0',
+    ])
   })
 })
