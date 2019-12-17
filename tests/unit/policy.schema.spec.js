3737a2d9dac5f93a4b2c41fc8bbd8bb1371c486f
Rowan Udell <rowan.udell@versent.com.au>
2019-12-18 08:23:29 +1100

    WIP basic policy json schema finished.

--- tests/unit/policy.schema.spec.js
+++ tests/unit/policy.schema.spec.js
@@ -11,4 +11,20 @@ describe.only('policy.schema.json', () => {
     if (!valid) console.log(validate.errors)
     expect(validate(AdministratorAccess)).toEqual(true)
   })
+
+  it('validates there is an Action', async () => {
+    const Invalid = require('./InvalidNoAction.policy.json')
+    const validate = ajv.compile(schema)
+    const valid = validate(Invalid)
+    // if (!valid) console.log(validate.errors)
+    expect(valid).toEqual(false)
+  })
+
+  it('validates Actions are strings', async () => {
+    const Invalid = require('./InvalidAction.policy.json')
+    const validate = ajv.compile(schema)
+    const valid = validate(Invalid)
+    // if (!valid) console.log(validate.errors)
+    expect(valid).toEqual(false)
+  })
 })
