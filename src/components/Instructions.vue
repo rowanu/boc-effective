f2a85e608302739742b9036311659849604c5768
rowan <rowanu@gmail.com>
2019-12-21 21:28:15 +1100

    docs: Include example policy and link to AWS Managed Policies. #5

--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -2,8 +2,41 @@
   <div id="instructions">
     <h1>Instructions</h1>
     <p>
-      Put some JSON in.
+      Paste or edit
     </p>
+    <h2>Example</h2>
+    <p>
+      Here's an example policy. AWSLambdaBasicExecutionRole
+    </p>
+
+    <pre>
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+      </pre
+    >
+    <h2>AWS Managed Policies</h2>
+    <div>
+      To see what the AWS Managed Policies give you, see this
+      community-maintained list:
+
+      <a href="https://github.com/glassechidna/trackiam/tree/master/policies">
+        AWS Managed Policies</a
+      >
+
+      Note that actual policy is in the "Document" field of the recorded data.
+    </div>
   </div>
 </template>
 
