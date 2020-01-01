45bbd24f466ab3cafb6fcea19161949ca655ceb4
rowan <rowanu@gmail.com>
2020-01-01 21:21:14 +1100

    refactor: Update instructions

--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -2,7 +2,14 @@
   <div id="instructions" class="container">
     <h2 class="title is-2">Instructions</h2>
     <p class="content">
-      To learn more about IAM Policies, check out
+      After you have input your policy JSON, you will see a list of allowed
+      actions by resource; permissions in AWS require an
+      <strong>explict allow</strong> to be permitted.
+    </p>
+
+    <p class="content">
+      You can also use "NotAction" and "Deny" to craft a specific set of
+      actoins. To learn more about IAM Policies, check out
       <a
         href="https://aws.amazon.com/blogs/security/back-to-school-understanding-the-iam-policy-grammar/"
         target="_blank"
@@ -10,8 +17,22 @@
       >.
     </p>
     <p class="content">
-      Here are some example policies.
+      Here are some example policies you can use to see action expansion.
     </p>
+
+    <h4 class="title is-4">AdministratorAccess</h4>
+    <pre class="content is-small">
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
+}</pre
+    >
 
     <h4 class="title is-4">AWSLambdaBasicExecutionRole</h4>
     <pre class="content is-small">
@@ -34,20 +55,6 @@
     }
   ],
   "Version": "2012-10-17"
-}</pre
-    >
-
-    <h4 class="title is-4">AdministratorAccess</h4>
-    <pre class="content is-small">
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "*",
-            "Resource": "*"
-        }
-    ]
 }</pre
     >
 
